040661e0f6dd518037bdac4ab850b744bb63ecec
Reehan Islam <reehan.akter@gmail.com>
2023-08-24 21:02:18 -0400

    Added a route

--- toggle.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for mobile navigation toggle
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
